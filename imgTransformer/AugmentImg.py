b0eb04465f8efc9067318b8d73777b0aecdda87f
Daksh Varshneya <daksh@Aganithas-MacBook-Air.local>
2017-08-30 16:39:58 +0530

    added batch mode

--- imgTransformer/AugmentImg.py
+++ imgTransformer/AugmentImg.py
@@ -18,35 +18,40 @@ class TransformImg():
 		h,w = img.shape[0],img.shape[1]
 		return img
 
-	def translate_x(self,img,tx_range):
+	def translate_x(self,img,tx_range,M):
 
 		tx = random.randint(tx_range[0],tx_range[1])
-		M = np.float32([[1,0,tx],[0,1,0]])
+		if M is None:
+			M = np.float32([[1,0,tx],[0,1,0]])
 		dst = cv2.warpAffine(img,M,(img.shape[1],img.shape[0]))
 		return dst,M
 
-	def translate_y(self,img,ty_range):
+	def translate_y(self,img,ty_range,M):
 
 		ty = random.randint(ty_range[0],ty_range[1])
-		M = np.float32([[1,0,0],[0,1,ty]])
+		if M is None:
+			M = np.float32([[1,0,0],[0,1,ty]])
 		dst = cv2.warpAffine(img,M,(img.shape[1],img.shape[0]))
 		return dst,M
 
-	def rotate(self,img,angle_range,centre):
+	def rotate(self,img,angle_range,centre,M):
 
 		angle = random.randint(angle_range[0],angle_range[1])
 		h,w = img.shape[0],img.shape[1]
-		M = cv2.getRotationMatrix2D(centre,angle,1)
+		if M is None:
+			M = cv2.getRotationMatrix2D(centre,angle,1)
 		dst = cv2.warpAffine(img,M,(w,h))
 		return dst,M
 
-	def shear(self,img,shear_range):
+	def shear(self,img,shear_range,M):
 		h,w = img.shape[0],img.shape[1]
 		pts1 = np.float32([[5,5],[20,5],[5,20]])
 		pt1 = 5+shear_range*np.random.uniform()-shear_range/2
 		pt2 = 20+shear_range*np.random.uniform()-shear_range/2
 		pts2 = np.float32([[pt1,5],[pt2,pt1],[5,pt2]])
-		M = cv2.getAffineTransform(pts1,pts2)
+
+		if M is None:
+			M = cv2.getAffineTransform(pts1,pts2)
 		dst = cv2.warpAffine(img,M,(w,h))
 
 		return dst,M
--- imgTransformer/Transformer.py
+++ imgTransformer/Transformer.py
@@ -1,6 +1,7 @@
 from AugmentImg import TransformImg
 from AugmentBox import TransformBox
 import random
+import numpy as np
 
 class Transformer():
 	def __init__(self,transform_boxes = False):
@@ -18,7 +19,7 @@ class Transformer():
 		self.shear_range = shear_range
 
 
-	def callTransform(self,img,idx,boxes=None):
+	def callTransform(self,img,idx,boxes=None,transformationMatrix=None):
 		if self.transform_boxes:
 			if boxes == None:
 				print 'Boxes not supplied'
@@ -47,19 +48,23 @@ class Transformer():
 			'''
 			Translate in x
 			'''
-			img_clone, M_tx = self.img_transformer.translate_x(img_clone,self.tx_range)
+			img_clone, M_tx = self.img_transformer.translate_x(img_clone,self.tx_range,transformationMatrix)
 
 			if self.transform_boxes:
 				boxes = self.box_transformer.getTransformedBoxes(boxes,M_tx)
+
+			transformationMatrix = M_tx
 
 		elif idx == 3:
 			'''
 			Translate in y
 			'''
-			img_clone, M_ty = self.img_transformer.translate_y(img_clone,self.ty_range)
+			img_clone, M_ty = self.img_transformer.translate_y(img_clone,self.ty_range,transformationMatrix)
 
 			if self.transform_boxes:
 				boxes = self.box_transformer.getTransformedBoxes(boxes,M_ty)
+
+			transformationMatrix = M_ty
 
 		elif idx == 4:
 			'''
@@ -68,19 +73,23 @@ class Transformer():
 			center_points = [(0,0),(w/2,h/2)]
 			center_idx = random.randint(0,1)
 
-			img_clone, M_rot = self.img_transformer.rotate(img_clone,self.rot_angle_range,center_points[center_idx])
+			img_clone, M_rot = self.img_transformer.rotate(img_clone,self.rot_angle_range,center_points[center_idx],transformationMatrix)
 			if self.transform_boxes:
 				boxes = self.box_transformer.getTransformedBoxes(boxes,M_rot)
+
+			transformationMatrix = M_rot
 
 		elif idx == 5:
 			'''
 			Shear
 			'''
-			img_clone, M_shear = self.img_transformer.shear(img_clone,self.shear_range)
+			img_clone, M_shear = self.img_transformer.shear(img_clone,self.shear_range,transformationMatrix)
 			if self.transform_boxes:
 				boxes = self.box_transformer.getTransformedBoxes(boxes,M_shear)
 
-		return img_clone,boxes
+			transformationMatrix = M_shear
+
+		return img_clone,boxes,transformationMatrix
 
 	def applyTransforms(self,img,boxes=None,num_transforms=2):
 		img_clone = img.copy()
@@ -88,8 +97,50 @@ class Transformer():
 
 		for transform_idx in range(num_transforms):
 			idx = random.randint(0,self.transforms)
-			img_clone,boxes = self.callTransform(img_clone,idx,boxes)
+			img_clone,boxes,_ = self.callTransform(img_clone,idx,boxes)
 		if boxes == None:
 			return img_clone
 		else:
 			return img_clone,boxes
+
+	def applyTransforms_batch(self,img_batch,boxes_batch=None,num_transforms=2):
+		
+		numImages = img_batch.shape[0]
+		# outImages = np.zeros(img_batch.shape)
+		outImages = img_batch.copy()
+
+		outBoxes = None
+		if self.transform_boxes:
+			# outBoxes = np.zeros(boxes_batch.shape)
+			outBoxes = boxes_batch.copy()
+
+		for transform_idx in range(num_transforms):
+
+			transformationMatrix = None
+			transformIdx = random.randint(0,self.transforms)
+
+			for imgIndex in range(0,numImages):
+				img = outImages[imgIndex]
+
+				if self.transform_boxes:
+					boxes = outBoxes[imgIndex]
+				else:
+					boxes = None
+				
+				img_clone = img.copy()
+
+				# print transformIdx, transformationMatrix
+
+				img_clone,boxes,transformationMatrix = self.callTransform(img_clone,transformIdx,boxes,
+					transformationMatrix = transformationMatrix)
+
+				if self.transform_boxes:
+					outBoxes[imgIndex,...] = boxes
+
+				outImages[imgIndex,...] = img_clone
+
+		if not self.transform_boxes:
+			return outImages
+		else:
+			return outImages,outBoxes
+
